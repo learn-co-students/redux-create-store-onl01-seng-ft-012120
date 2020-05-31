04423109e06dca46aea5ca5e422fb512fa0248c6
Damon Marc Rocha <dmarcr1997@gmail.com>
2020-05-31 15:34:09 -0500

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,41 @@
-let state;
+function createStore(reducer) {
+  let state;
 
-function reducer(state = { count: 0 }, action) {
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return { count: state.count + 1 };
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  };
 
-    default:
-      return state;
-  }
-};
+  function getState(){
+    return state;
+  };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+  return { 
+    dispatch,
+    getState
+  };
+}
+  let store = createStore(reducer)
+  function reducer(state = { count: 0 }, action) {
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return { count: state.count + 1 };
 
-function render() {
-  let container = document.getElementById('container');
-  container.textContent = state.count;
-};
+      default:
+        return state;
+    }
+  };
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
+ 
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+  function render() {
+    let container = document.getElementById('container');
+    container.textContent = store.getState().count;
+  };
+
+  store.dispatch({ type: '@@INIT' })
+  let button = document.getElementById('button');
+
+  button.addEventListener('click', function() {
+      store.dispatch({ type: 'INCREASE_COUNT' });
+  })
